d704bcbcf2a5f443bbc5483edde4f268bda8ad8c
AnuXi313 <anuxi313@gmail.com>
2025-07-16 21:45:37 +0330

    remove txt file

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,7 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
-import { viteStaticCopy } from "vite-plugin-static-copy";
-
 export default defineConfig({
-	plugins: [
-		tailwindcss(),
-		viteStaticCopy({
-			targets: [
-				{
-					src: "46787.txt",
-					dest: "", // کپی در ریشه dist/
-				},
-			],
-		}),
-	],
+	plugins: [tailwindcss()],
 
 	base: "/",
 
